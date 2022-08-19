2852263ded4cace17b3905cc81eb932ab6c4decb
Robson da Silva Leal <robson_leal@sicredi.com.br>
2022-08-19 11:29:00 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/sicredi/comercio/sicredicomercio/SicrediComercioApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi.comercio.sicredicomercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrediComercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicrediComercioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sicredi/comercio/sicredicomercio/SicrediComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.comercio.sicredicomercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrediComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
